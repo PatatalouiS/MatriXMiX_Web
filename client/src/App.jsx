a62d06d3c872a9bdbf304f0093b4ede46a152d8a
Maxime OLIVIE <maxime.olivie@etu.univ-lyon1.fr>
2020-06-02 04:00:00 +0200

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './index.css';
+import logo from './img/logo.png';
+
+const App = () => {
+	return (
+		<div id='logo-container'>
+			<img src={logo} className='center' id='logo'/>
+		</div>
+		
+	);
+};
+
+export default App;
