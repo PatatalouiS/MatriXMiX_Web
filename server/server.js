4f1862cb70bdaf043fe7532df903120ab581e221
OLIVIE MAXIME p1710179 <maxime.olivie@etu.univ-lyon1.fr>
2020-06-29 21:00:53 +0200

    before eject

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const dotenv = require('dotenv').config();
 
 server.use(cors());
 server.use(bodyParser.json());
-server.use(express.static(path.join(__dirname, 'app')));
 
 // Custom middleware, for logging input routes and set more comfortably the
 // request object
@@ -21,6 +20,22 @@ server.use((req, res, next) => {
     }
     next();
 });
+
+// for gzip encoding
+server.use('*.js', (req, res, next) => {
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'text/javascript');
+    next();
+});
+
+//for gzip encoding
+server.use('*.css', (req, res, next) => {
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'text/css');
+    next();
+});
+
+server.use(express.static(path.join(__dirname, 'app')));
 
 server.use('/api/test/', testRouter);
 server.use('/api/calc/', calcRouter);
